94a385c27c27b86e447e8705c2a144e1a7235167
praisepraise65@gmail.com <praisepraise65@gmail.com>
2023-09-27 23:42:11 +0000

    Javascript promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,13 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  // Create an array of promises
-  const promises = [uploadPhoto(), createUser()];
-
-  // Resolve all promises and log the required information
-  Promise.all(promises)
-    .then(([photoResponse, userResponse]) => {
-      const { body: { firstName, lastName } } = userResponse;
-      console.log(`${firstName} ${lastName}`);
-    })
-    .catch((error) => {
-      console.log('Signup system offline');
-    });
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((values) => {
+    const [uploadPhoto, createUser] = values;
+    console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
+  }).catch(() => console.log('Signup system offline'));
 }
 
+export default handleProfileSignup;
